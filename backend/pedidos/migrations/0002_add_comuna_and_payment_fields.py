2412713be91749275a818571aa48a861656f68d7
Rektres <mateo.aramedi@gmail.com>
2026-08-26 00:35:36 -0400

    fix: ajustar migracion 0002 de pedidos a solo campo comuna

--- backend/pedidos/migrations/0002_add_comuna_and_payment_fields.py
+++ backend/pedidos/migrations/0002_add_comuna_and_payment_fields.py
@@ -1,4 +1,4 @@
-﻿from django.db import migrations, models
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
@@ -13,29 +13,5 @@ class Migration(migrations.Migration):
             name='comuna',
             field=models.CharField(blank=True, max_length=100),
         ),
-        migrations.AddField(
-            model_name='pedido',
-            name='metodo_pago',
-            field=models.CharField(choices=[('mercadopago', 'Mercado Pago'), ('transferencia', 'Transferencia Bancaria')], default='mercadopago', max_length=20),
-        ),
-        migrations.AddField(
-            model_name='pedido',
-            name='transaccion_id',
-            field=models.CharField(blank=True, max_length=100),
-        ),
-        migrations.AddField(
-            model_name='pedido',
-            name='url_pago',
-            field=models.TextField(blank=True),
-        ),
-        migrations.AddField(
-            model_name='pedido',
-            name='pagado_en',
-            field=models.DateTimeField(blank=True, null=True),
-        ),
-        migrations.AddField(
-            model_name='pedido',
-            name='datos_pago_raw',
-            field=models.JSONField(blank=True, default=dict),
-        ),
     ]
+
